c9af4cdbc2bd2715eb88702fd4453c49993512a5
GustavoLopesMoreira <GustavoLMConta@Gmail.Com>
2025-05-10 18:43:46 -0300

    termino
    projeto  completo, faltando agora os acabamentos

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ if (nome == null) {
 } else {
     div.innerText += `Olá, ${nome}!
                 Meu nome é Gustavo Lopes Moreira. Seja bem-vindo ao meu site!
-                Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida. Resido no Méier, bairro localizado na cidade do Rio de Janeiro.
+                Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida. 
+                Resido no Méier, bairro localizado na cidade do Rio de Janeiro.
                 Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
-                Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também
-                estão sendo desenvolvidas por meio da formação acadêmica.`
+                Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também estão sendo desenvolvidas por meio da formação acadêmica.`
 }
