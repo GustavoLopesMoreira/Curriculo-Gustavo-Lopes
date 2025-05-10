14cdbc52567912bf51bf174eb0689a38d982b376
GustavoLopesMoreira <GustavoLMConta@Gmail.Com>
2025-05-10 16:26:31 -0300

    apresentação
    fiz uma apresentação inicial com javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = prompt('Seja bem-vindo! Qual é o seu nome?')
+var div = document.querySelector(`div#itemsmain`)
+div.innerText += `Olá, ${name}!
+                    Meu nome é Gustavo Lopes Moreira. Seja bem-vindo ao meu site!
+                    Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade
+                    Veiga de Almeida. Resido no Méier, bairro localizado na cidade do Rio de Janeiro.
+                    Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
+                    Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também
+                    estão sendo desenvolvidas por meio da formação acadêmica.`
