dc3a4800ffc196c2d1945145c8e56b535c167347
GustavoLopesMoreira <GustavoLMConta@Gmail.Com>
2025-05-10 16:47:33 -0300

    pequena correçao
    adicionei uma opçao para null no formulario inicial

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var name = prompt('Seja bem-vindo! Qual é o seu nome?')
+var nome = prompt('Seja bem-vindo! Qual é o seu nome?')
 var div = document.querySelector(`div#itemsmain`)
-div.innerText += `Olá, ${name}!
-                    Meu nome é Gustavo Lopes Moreira. Seja bem-vindo ao meu site!
-                    Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade
-                    Veiga de Almeida. Resido no Méier, bairro localizado na cidade do Rio de Janeiro.
-                    Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
-                    Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também
-                    estão sendo desenvolvidas por meio da formação acadêmica.`
+if (nome == null) {
+    div.innerText += `Olá,  Seja bem-vindo ao meu site!
+                Meu nome é Gustavo Lopes Moreira.Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida.
+                Resido no Méier, bairro localizado na cidade do Rio de Janeiro. Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
+                Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também estão sendo desenvolvidas por meio da formação acadêmica.`
+} else {
+    div.innerText += `Olá, ${nome}!
+                Meu nome é Gustavo Lopes Moreira. Seja bem-vindo ao meu site!
+                Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida. Resido no Méier, bairro localizado na cidade do Rio de Janeiro.
+                Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
+                Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também
+                estão sendo desenvolvidas por meio da formação acadêmica.`
+}
