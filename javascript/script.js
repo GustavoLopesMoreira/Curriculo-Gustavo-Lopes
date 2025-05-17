a75941c5e6dda4a5e9c8c2a412c04a5e39f99cc6
GustavoLopesMoreira <GustavoLMConta@Gmail.Com>
2025-05-16 21:47:56 -0300

    remake
    refazendo a estruturação do codigo utilizando boas praticas e melhor otimização

--- javascript/script.js
+++ javascript/script.js
@@ -3,13 +3,12 @@ var div = document.querySelector(`div#itemsmain`)
 if (nome == null) {
     div.innerText += `Olá,  Seja bem-vindo ao meu site!
                 Meu nome é Gustavo Lopes Moreira.Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida.
-                Resido no Méier, bairro localizado na cidade do Rio de Janeiro. Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
+                Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
                 Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também estão sendo desenvolvidas por meio da formação acadêmica.`
 } else {
     div.innerText += `Olá, ${nome}!
                 Meu nome é Gustavo Lopes Moreira. Seja bem-vindo ao meu site!
                 Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida. 
-                Resido no Méier, bairro localizado na cidade do Rio de Janeiro.
                 Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
                 Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também estão sendo desenvolvidas por meio da formação acadêmica.`
 }
