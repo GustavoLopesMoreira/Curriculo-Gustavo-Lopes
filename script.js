a5cf55bac433f70a80929100369980dc4f49f3ae
GustavoLopesMoreira <GustavoLMConta@Gmail.Com>
2025-05-25 12:05:41 -0300

    estruturação
    arrumei alguns erros na versão mobile e estruturei o codigo de maneira mais otimizada

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 var nome = prompt('Seja bem-vindo! Qual é o seu nome?')
-var div = document.querySelector(`div#itemsmain`)
+var div = document.querySelector(`div#textomain`)
 if (nome == null) {
     div.innerText += `Olá,  Seja bem-vindo ao meu site!
-                Meu nome é Gustavo Lopes Moreira.Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida.
-                Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
-                Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também estão sendo desenvolvidas por meio da formação acadêmica.`
+                Meu nome é Gustavo Lopes Moreira.Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida.Este site foi criado para apresentar e divulgar minhas habilidades em programação`
 } else {
     div.innerText += `Olá, ${nome}!
                 Meu nome é Gustavo Lopes Moreira. Seja bem-vindo ao meu site!
-                Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida. 
-                Tenho me dedicado ao estudo de programação há dois anos, inicialmente por meio de cursos online.
-                Este site foi criado para apresentar e divulgar minhas habilidades em programação, que agora também estão sendo desenvolvidas por meio da formação acadêmica.`
+                Tenho 24 anos e sou estudante do primeiro período do curso de Ciência da Computação na Universidade Veiga de Almeida. asEste site foi criado para apresentar e divulgar minhas habilidades em programação.`
 }
 
 
